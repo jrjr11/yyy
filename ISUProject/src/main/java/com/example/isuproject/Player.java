05dec0075f4d4c532888b8c0a5d7c816869a625a
jrjr11 <131634747+jrjr11@users.noreply.github.com>
2023-04-29 19:39:00 -0400

    Update Player.java

--- ISUProject/src/main/java/com/example/isuproject/Player.java
+++ ISUProject/src/main/java/com/example/isuproject/Player.java
@@ -1,80 +1,119 @@
 package com.example.isuproject;
+
 import java.awt.*;
 
-public class Player extends Entity
+public class Wall
 {
-    public Player(int x, int y, int dx, int dy, int speed, Color color)
-    {
-        super(x, y, dx, dy, speed, color);
+    private int x;
+    private int y;
+    private boolean rightD;
+    private boolean leftD;
+    private boolean upD;
+    private boolean downD;
+    private boolean isDead;
 
+    public Wall(int x, int y, boolean r, boolean l, boolean u, boolean d, boolean dead)
+    {
+        this.x = x;
+        this.y = y;
+        rightD = r;
+        leftD = l;
+        upD = u;
+        downD = d;
+        isDead = dead;
     }
+    public int getX() {return x;}
+    public int getY() {return y;}
+    public boolean getRightD() {return rightD;}
+    public boolean getLeftD() {return leftD;}
+    public boolean getUpD() {return upD;}
+    public boolean getDownD() {return downD;}
+    public boolean getIsDead() {return isDead;}
 
-    public void moveRight()
-    {
-        if(getY() % 50 == 0){    //If within Row
-            setX(getX()+ getSpeed());
-            setDy(0);
-            setDx(+1);
-        }
-        else if(getY() % 50 == 5 || getY() % 50 == 10){ //Leeway within grid
-            setY(getY()-5);
-        }
-        else if(getY() % 50 == 40 || getY() % 50 == 45){ //Leeway within grid
-            setY(getY()+5);
-        }
-        System.out.println("XLOC: "+getX());
-    }
-    public void moveLeft()
-    {
-        if(getY() % 50 == 0){    //If within Row
-            setX(getX()- getSpeed());
-            setDy(0);
-            setDx(-1);
-        }
-        else if(getY() % 50 == 5 || getY() % 50 == 10){ //Leeway within grid
-            setY(getY()-5);
-        }
-        else if(getY() % 50 == 40 || getY() % 50 == 45){ //Leeway within grid
-            setY(getY()+5);
-        }
-        System.out.println("XLOC: "+getX());
-    }
-    public void moveUp()
-    {
-        if(getX() % 50 == 0){    //If within column
-            setY(getY()- getSpeed());
-            setDx(0);
-            setDy(-1);
+    public void setDead(boolean is) {isDead = is;}
+    public void setRightD(boolean rD) {rightD = rD;}
+    public void setLeftD(boolean lD) {leftD = lD;}
+    public void setUpD(boolean uD) {upD = uD;}
+    public void setDownD(boolean dD) {downD = dD;}
 
-        }
-        else if(getX() % 50 == 5 || getX() % 50 == 10){ //Leeway within grid
-            setX(getX()-5);
-        }
-        else if(getX() % 50 == 40 || getX() % 50 == 45){ //Leeway within grid
-            setX(getX()+5);
-        }
-        System.out.println("YLOC: "+getY());
-    }
-    public void moveDown()
+
+    public String toString()
     {
-        if(getX() % 50 == 0){    //If within column
-            setY(getY()+ getSpeed());
-            setDx(0);
-            setDy(1);
-        }
-        else if(getX() % 50 == 5 || getX() % 50 == 10){ //Leeway within grid
-            setX(getX()-5);
-        }
-        else if(getX() % 50 == 40 || getX() % 50 == 45){ //Leeway within grid
-            setX(getX()+5);
-        }
-        System.out.println("YLOC: "+getY());
+        return "X: " + x + "\tY: " + y + "\tR: " + rightD + "\tL: " + leftD + "\tU: " + upD
+                + "\tD: " + downD + "\tDDDD: " +downD;
     }
     public void draw(Graphics g)
     {
-        g.setColor(Color.black);
-        g.fillRoundRect(getX()+5, getY()+5, 41, 41, 10, 10);
-        g.setColor(getColor());
-        g.fillOval(getX()+10, getY()+10, 30, 30);
+        g.setColor(Color.yellow);
+
+        if(y>250)
+        {
+            g.setColor(Color.orange);
+        }
+        if(y>450)
+        {
+            g.setColor(Color.orange.darker());
+        }
+        if(y>650)
+        {
+            g.setColor(Color.red.darker());
+        }
+        g.fillRect(x, y, 50, 50);
+        g.setColor(Color.yellow.darker());
+        if(y>250)
+        {
+            g.setColor(Color.orange.darker());
+        }
+        if(y>450)
+        {
+            g.setColor(Color.orange.darker().darker());
+        }
+        if(y>650)
+        {
+            g.setColor(Color.red.darker().darker());
+        }
+        g.fillRect(x+27, y+2, 3, 3);
+        g.fillRect(x+20, y+9, 3, 3);
+        g.fillRect(x+45, y+9, 3, 3);
+        g.fillRect(x+8, y+14, 3, 3);
+        g.fillRect(x+20, y+19, 3, 3);
+        g.fillRect(x+29, y+14, 3, 3);
+        g.fillRect(x+12, y+23, 3, 3);
+        g.fillRect(x+40, y+25, 3, 3);
+        g.fillRect(x+19, y+27, 3, 3);
+        g.fillRect(x+12, y+34, 3, 3);
+        g.fillRect(x+42, y+34, 3, 3);
+        g.fillRect(x+10, y+45, 3, 3);
+        g.fillRect(x+25, y+42, 3, 3);
+        g.fillRect(x+34, y+45, 3, 3);
+        g.fillRect(x+31, y+30, 3, 3);
+        g.fillRect(x+3, y+25, 3, 3);
+        g.fillRect(x+5, y+4, 3, 3);
+        g.fillRect(x+46, y+46, 3, 3);
+        if (isDead == true)
+        {
+            g.setColor(Color.black);
+            g.fillRoundRect(x+5, y+5, 41, 41, 8, 8);
+        }
+        if (rightD == true)
+        {
+            g.setColor(Color.black);
+            g.fillRoundRect(x+30, y+5, 30, 41, 8, 8);
+        }
+        if (leftD == true)
+        {
+            g.setColor(Color.black);
+            g.fillRoundRect(x-10, y+5, 30, 41, 8,8);
+        }
+        if (upD == true)
+        {
+            g.setColor(Color.black);
+            g.fillRoundRect(x+5, y-5, 41, 25, 8, 8);
+        }
+        if (downD == true)
+        {
+            g.setColor(Color.black);
+            g.fillRoundRect(x+5, y+30, 41, 25, 8, 8);
+        }
     }
 }
