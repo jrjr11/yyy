19940c03025e3bee2b0644493f61cf336965ea90
jrjr11 <131634747+jrjr11@users.noreply.github.com>
2023-05-19 16:35:33 -0400

    Update Player.java

--- ISUProject/src/main/java/com/example/isuproject/Player.java
+++ ISUProject/src/main/java/com/example/isuproject/Player.java
@@ -1,4 +1,4 @@
-package com.example.isuproject;
+package com.example.isuproject1;
 import java.awt.*;
 
 public class Player extends Entity
@@ -11,70 +11,78 @@ public class Player extends Entity
 
     public void moveRight()
     {
-        if(getY() % 50 == 0){    //If within Row
-            setX(getX()+ getSpeed());
-            setDy(0);
-            setDx(+1);
+        if(getX() < 650)
+        {
+            if(getY() % 50 == 0){    //If within Row
+                setX(getX()+ getSpeed());
+                setDy(0);
+                setDx(+1);
+            }
+            else if(getY() % 50 == 5 || getY() % 50 == 10){ //Leeway within grid
+                setY(getY()-5);
+            }
+            else if(getY() % 50 == 40 || getY() % 50 == 45){ //Leeway within grid
+                setY(getY()+5);
+            }
         }
-        else if(getY() % 50 == 5 || getY() % 50 == 10){ //Leeway within grid
-            setY(getY()-5);
-        }
-        else if(getY() % 50 == 40 || getY() % 50 == 45){ //Leeway within grid
-            setY(getY()+5);
-        }
-        System.out.println("XLOC: "+getX());
+
     }
     public void moveLeft()
     {
-        if(getY() % 50 == 0){    //If within Row
-            setX(getX()- getSpeed());
-            setDy(0);
-            setDx(-1);
+        if(getX() > 0)
+        {
+            if(getY() % 50 == 0){    //If within Row
+                setX(getX()- getSpeed());
+                setDy(0);
+                setDx(-1);
+            }
+            else if(getY() % 50 == 5 || getY() % 50 == 10){ //Leeway within grid
+                setY(getY()-5);
+            }
+            else if(getY() % 50 == 40 || getY() % 50 == 45){ //Leeway within grid
+                setY(getY()+5);
+            }
         }
-        else if(getY() % 50 == 5 || getY() % 50 == 10){ //Leeway within grid
-            setY(getY()-5);
-        }
-        else if(getY() % 50 == 40 || getY() % 50 == 45){ //Leeway within grid
-            setY(getY()+5);
-        }
-        System.out.println("XLOC: "+getX());
+
     }
     public void moveUp()
     {
-        if(getX() % 50 == 0){    //If within column
-            setY(getY()- getSpeed());
-            setDx(0);
-            setDy(-1);
+        if(getY() > 0) {
+            if (getX() % 50 == 0) {    //If within column
+                setY(getY() - getSpeed());
+                setDx(0);
+                setDy(-1);
 
+            } else if (getX() % 50 == 5 || getX() % 50 == 10) { //Leeway within grid
+                setX(getX() - 5);
+            } else if (getX() % 50 == 40 || getX() % 50 == 45) { //Leeway within grid
+                setX(getX() + 5);
+            }
         }
-        else if(getX() % 50 == 5 || getX() % 50 == 10){ //Leeway within grid
-            setX(getX()-5);
-        }
-        else if(getX() % 50 == 40 || getX() % 50 == 45){ //Leeway within grid
-            setX(getX()+5);
-        }
-        System.out.println("YLOC: "+getY());
     }
     public void moveDown()
     {
-        if(getX() % 50 == 0){    //If within column
-            setY(getY()+ getSpeed());
-            setDx(0);
-            setDy(1);
+        if(getY() < 800)
+        {
+            if (getX() % 50 == 0) {    //If within column
+                setY(getY() + getSpeed());
+                setDx(0);
+                setDy(1);
+            } else if (getX() % 50 == 5 || getX() % 50 == 10) { //Leeway within grid
+                setX(getX() - 5);
+            } else if (getX() % 50 == 40 || getX() % 50 == 45) { //Leeway within grid
+                setX(getX() + 5);
+            }
         }
-        else if(getX() % 50 == 5 || getX() % 50 == 10){ //Leeway within grid
-            setX(getX()-5);
-        }
-        else if(getX() % 50 == 40 || getX() % 50 == 45){ //Leeway within grid
-            setX(getX()+5);
-        }
-        System.out.println("YLOC: "+getY());
     }
     public void draw(Graphics g)
     {
-        g.setColor(Color.black);
-        g.fillRoundRect(getX()+5, getY()+5, 41, 41, 10, 10);
         g.setColor(getColor());
         g.fillOval(getX()+10, getY()+10, 30, 30);
+        if(getColor() == Color.magenta)
+        {
+            g.setColor(Color.red);
+            g.fillRect(0, 0, 1000, 1000);
+        }
     }
 }
