9627f72e988c29e65777bc66b737912df3a15738
jrjr11 <131634747+jrjr11@users.noreply.github.com>
2023-05-19 16:33:34 -0400

    Update Enemy.java

--- ISUProject/src/main/java/com/example/isuproject/Enemy.java
+++ ISUProject/src/main/java/com/example/isuproject/Enemy.java
@@ -1,27 +1,32 @@
-package com.example.isuproject;
+package com.example.isuproject1;
 
 import java.awt.*;
 
 public class Enemy extends Entity
 {
-    public Enemy(int x, int y, int dx, int dy, int speed, Color color)
+    private boolean alive;
+    public Enemy(int x, int y, int dx, int dy, int speed, Color color, boolean alive1)
     {
         super(x, y, dx, dy, speed, color);
+        alive = alive1;
 
     }
+    public void setAlive(boolean alive1){alive = alive1;};
+    public boolean getAlive(){return alive;};
+
     public void moveNext()
     {
         if(getY() % 50 == 5 || getY() % 50 == 10){ //Leeway within grid
-        setY(getY()-5);
+            setY(getY()-5);
         }
         else if(getY() % 50 == 40 || getY() % 50 == 45){ //Leeway within grid
-        setY(getY()+5);
+            setY(getY()+5);
         }
     }
-    public void move(Maze maze, Wall[][] map, Player p)
-    {
+    public void move(Maze maze, Wall[][] map, Player p) {
         int x = (getX()+25)/50;
-        int y = (getY()-25)/50;
+        int y = (getY() - 25) / 50;
+
         maze = new Maze(14, 16, map, p);
         int[][] solved = maze.getGrid();
         MazeSolver solver = new MazeSolver(maze);
@@ -31,52 +36,110 @@ public class Enemy extends Entity
             {
                 if(solved[x-1][y] == 3)
                 {
-                    if(getY() % 50 == 0)
-                        setX(getX()-5);
+                    if(getY() % 50 == 0&& map[x][y].getLeftD() == true) {
+                        setX(getX() - 5);
+                        setDx(-1);
+
+                        setDy(0);
+                    }
+                    else if(getY() % 50 != 0)
+                    {
+                        if(getDy() == 1)
+                        {
+                            setY(getY()+5);
+                        }
+                        else if(getDy() == -1)
+                            setY(getY()-5);
+                    }
                 }
             }
             if (x != 13)
             {
                 if(solved[x+1][y] == 3)
                 {
-                    if(getY() % 50 == 0)
+                    if(getY() % 50 == 0 && map[x][y].getRightD() == true){
                         setX(getX()+5);
+                        setDx(1);
+                        setDy(0);
+                    }
+                    else if(getY() % 50 != 0)
+                    {
+                        if(getDy() == 1)
+                        {
+                            setY(getY()+5);
+                        }
+                        else if(getDy() == -1)
+                            setY(getY()-5);
+                    }
                 }
             }
-            if(y != 0)
+            if(y != 0)// up
             {
                 if(solved[x][y-1] == 3)
                 {
-                    if(getX() % 50 == 0)
-                        setY(getY()-5);
-
+                    if(getX() % 50 == 0 && map[x][y].getUpD() == true) {
+                        setY(getY() - 5);
+                        setDy(-1);
+                        setDx(0);
+                    }
+                    else if(getX() % 50 != 0)
+                    {
+                        if(getDx() == 1)
+                        {
+                            setX(getX()+5);
+                        }
+                        else if (getDx() == -1)
+                            setX(getX()-5);
+                    }
                 }
             }
-            if(y != 15)
+            if(y != 15)// down
             {
                 if(solved[x][y+1] == 3)
                 {
-                    if(getX() % 50 == 0)
-                        setY(getY()+5);
+                    if(getX() % 50 == 0 && map[x][y].getDownD() == true) {
+                        setY(getY() + 5);
+                        setDy(1);
+                        setDx(0);
+                    }
+                    else if(getX() % 50 != 0)
+                    {
+                        if(getDx() == 1)
+                        {
+                            setX(getX()+5);
+                        }
+                        else if (getDx() == -1)
+                            setX(getX()-5);
+                    }
                 }
             }
-            //System.out.println("X: "+getX() % 50);
         }
-        //System.out.print(maze);
 
     }
     public void collidesWith(Player p)
     {
-        Rectangle enemy = new Rectangle(getX(), getY(), 41, 41);
-        Rectangle player = new Rectangle(p.getX(), p.getY(), 41, 41);
-        if (enemy.intersects(player))
+        if(getAlive() == true) {
+            Rectangle enemy = new Rectangle(getX(), getY(), 30, 30);
+            Rectangle player = new Rectangle(p.getX(), p.getY(), 30, 30);
+            if (enemy.intersects(player)) {
+                p.setColor(Color.magenta);
+            }
+        }
+    }
+    public void ifShot(Gun g)
+    {
+        Rectangle enemy = new Rectangle(getX(), getY(), 30, 30);
+        Rectangle gun = new Rectangle(g.getX(), g.getY(), 5, 5);
+        if (enemy.intersects(gun))
         {
-            p.setColor(Color.magenta);
+            setAlive(false);
+            setColor(Color.magenta);
         }
     }
     public void draw(Graphics g)
     {
-        g.setColor(getColor());
-        g.fillOval(getX()+10, getY()+10, 30, 30);
+            g.setColor(getColor());
+            g.fillOval(getX() + 10, getY() + 10, 30, 30);
+
     }
 }
